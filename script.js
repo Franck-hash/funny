8c790cd83dd9ebb86cf03dcde9c0e187f660dc90
Franck-hash <55457365+Franck-hash@users.noreply.github.com>
2020-10-12 16:04:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ $(document).ready(function() {
 		const number = parseInt($('.number').text());
 		if (number < 100) {
 			$('.number').text(number + 1);
+		} else {
+			$('.n-div, .caveman').fadeOut('slow');
+			setTimeout(function() {
+				$('.back').removeClass('back-one');
+				$('.bg-two').removeClass('bg-one');
+				$('.loader').remove();
+			}, 1000);
 		}
 	}, 50);
 
